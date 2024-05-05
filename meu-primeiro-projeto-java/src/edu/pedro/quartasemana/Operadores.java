4578f3a38bb994177930610123edc6b8f484fd18
Pedronovaes1 <pedro.novaes@souunit.com.br>
2024-05-05 19:53:58 -0300

    Aulas de variáveis e operadores

---
+++ meu-primeiro-projeto-java/src/edu/pedro/quartasemana/Operadores.java
@@ -0,0 +1,25 @@
+package edu.pedro.quartasemana;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+        
+        /*String resultado = " ";
+        if(a==b){
+           resultado = "Verdadeiro";
+        }else{
+            resultado = "falso";
+        }
+
+        System.out.println(resultado);*/
+       
+        //operadores ternários
+
+        String resultado = a==b? "verdadeiro" : "falso";
+        System.out.println(resultado);
+        
+    }
+}
---
+++ meu-primeiro-projeto-java/src/edu/pedro/terceirasemana/TiposVariaveis.java
@@ -0,0 +1,13 @@
+package edu.pedro.terceirasemana;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        final double PI = 3.14;
+    }
+}
