1cc91b3a88814a5ccd1fe878525318f60c07d8e3
Pedronovaes1 <pedro.novaes@souunit.com.br>
2024-05-04 21:52:28 -0300

    Primeira aula

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    public static void main(String[] args){
+        //Isso aqui é um método dentro de uma classe
+        System.out.println("olá, mundo");
+        //uma classe System, que tem vários recursos
+    }
+}
