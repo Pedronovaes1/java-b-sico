c56bdee74f5d0ff2852b15fb4f43551a12b61a65
Pedronovaes1 <pedro.novaes@souunit.com.br>
2024-05-06 22:48:17 -0300

    Finalizando o curso de repetição

---
+++ java-repetições/src/RepetiçãoMatriz.java
@@ -0,0 +1,9 @@
+public class RepetiçãoMatriz {
+    public static void main(String[] args) {
+        String aluno[] = {"Felipe", "Jonas", "Pedro"};
+
+        for(int x = 0; x < aluno.length; x++ ){
+            System.out.println("O aluno no indice x = "+ x + " é " + aluno[x]);
+        }
+    }
+}
